fcd504517a16b4da6f71d2be76afe16f902eff8f
henrique <henriquepermartins@gmail.com>
2024-08-25 20:34:49 -0300

    fix premium acess

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -1,20 +1,20 @@
 import { getSession } from "@/app/actions";
-import { redirect } from "next/dist/server/api-utils";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 const PremiumPage = async () => {
   const session = await getSession();
 
-  if (!session) {
+  if (!session.isLoggedIn) {
     redirect("/");
   }
 
   if (!session.isPro) {
     return (
       <div className="notPremium">
-        <h1>Only premium users can access this page</h1>
+        <h1>Only premium users can see the content!</h1>
         <Link href="/profile">
-          Go to the profile page to upgrade to premium.
+          Go to the profile page to upgrade to premium
         </Link>
       </div>
     );
@@ -22,14 +22,10 @@ const PremiumPage = async () => {
 
   return (
     <div className="premium">
-      <h1>Welcome to the premium page</h1>
-      <ul>
-        <li>You are a premium user</li>
-        <li>You can access premium features</li>
-        <li>And more premium features</li>
-      </ul>
+      <h1>Welcome to the PremiumPage</h1>
+      <p>Congratulations! You are a <b>premium user!</b></p>
     </div>
-  )
+  );
 };
 
 export default PremiumPage;
